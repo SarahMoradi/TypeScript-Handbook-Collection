ee500f1e90b48af8835e7969f443538fa22d1a4a
Sarah <moradisarah2001@gmail.com>
2026-02-27 15:05:45 +0330

    feat(the-basics): add basics example

--- The-Basics/index.js
+++ The-Basics/index.js
@@ -1,30 +1,21 @@
 // Static type-checking
-
-const message = "hello!";
+var _message = "hello!";
 // message();
 // This expression is not callable.
 // Type 'String' has no call signatures.
-
 //#################################################
-
 // Non-exception Failures
-const user = {
-  name: "Daniel",
-  age: 26,
+var _user = {
+    name: "Daniel",
+    age: 26,
 };
 // user.location;
 // Property 'location' does not exist on type '{ name: string; age: number; }'.
-
 //#################################################
-
 // Explicit Types
-function greet(person: string, date: Date) {
-  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+function _greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
 }
-
 // greet("Brendan", new Date());
 // greet("Brendan");
 // Expected 2 arguments, but got 1.
-
-// noImplicitAny
-// strictNullChecks
